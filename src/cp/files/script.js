ac5a83e2a6d7602849e026b3c31f2985b3db7059
Konstantin <kostya.svetashov.01@gmail.com>
2025-10-24 18:53:17 +0400

    feat: implement cp, wt, zip, streams

--- src/cp/files/script.js
+++ src/cp/files/script.js
@@ -1,19 +1,12 @@
-import { EOL } from 'node:os';
-import { argv, stdout, stdin, exit } from 'node:process';
-
-const args = argv.slice(2);
+const args = process.argv.slice(2);
 
 console.log(`Total number of arguments is ${args.length}`);
-console.log(`Arguments: ${JSON.stringify(args)}${EOL}`);
+console.log(`Arguments: ${JSON.stringify(args)}`);
 
 const echoInput = (chunk) => {
   const chunkStringified = chunk.toString();
-
-  if (chunkStringified.includes('CLOSE')) {
-    exit(0);
-  }
-
-  stdout.write(`Received from master process: ${chunk.toString()}${EOL}`);
+  if (chunkStringified.includes("CLOSE")) process.exit(0);
+  process.stdout.write(`Received from master process: ${chunk.toString()}\n`);
 };
 
-stdin.on('data', echoInput);
+process.stdin.on("data", echoInput);
