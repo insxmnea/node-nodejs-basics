13ab511c0eaf7fba0b96a1791900f5d3b1e68478
Konstantin <kostya.svetashov.01@gmail.com>
2025-11-01 17:58:14 +0400

    refactor: remove unused files

--- src/cli/env.js
+++
@@ -1,8 +0,0 @@
-const parseEnv = () => {
-  const rssVars = Object.entries(process.env)
-    .filter(([key]) => key.startsWith("RSS_"))
-    .map(([key, value]) => `${key}=${value}`);
-  console.log(rssVars.join("; "));
-};
-
-parseEnv();
